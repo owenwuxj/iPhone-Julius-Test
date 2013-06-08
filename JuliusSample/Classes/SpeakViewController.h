34329080b7c577a97f51b2f3791682e6f3415080
Michael Wang <Michael.WangCH@EF.com>
2013-06-08 10:05:44 +0800

    add SpeakViewController

---
+++ JuliusSample/Classes/SpeakViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpeakViewController.h
+//  JuliusSample
+//
+//  Created by Michael Wang on 13-6-8.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpeakViewController : UIViewController
+
+@end
