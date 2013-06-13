04d9a5a78e68ae8e24d8fb99d59c9fdc35f53390
Michael Wang <Michael.WangCH@EF.com>
2013-06-13 10:18:16 +0800

    Add SpeakView

---
+++ JuliusSample/Classes/SpeakView.h
@@ -0,0 +1,13 @@
+//
+//  SpeakView.h
+//  JuliusSample
+//
+//  Created by Michael Wang on 13-6-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpeakView : UIView
+
+@end
---
+++ JuliusSample/Classes/SpeakView.m
@@ -0,0 +1,31 @@
+//
+//  SpeakView.m
+//  JuliusSample
+//
+//  Created by Michael Wang on 13-6-13.
+//
+//
+
+#import "SpeakView.h"
+
+@implementation SpeakView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
